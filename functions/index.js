89c47da20d846d6439dd9f302306fcb072cb8e55
kovmike <kovmike@yandex.ru>
2021-03-01 20:34:47 +0300

    arrow func

--- functions/index.js
+++ functions/index.js
@@ -8,28 +8,18 @@ const KEYFILE = "configCalendar.json"; // path to JSON with private key been dow
 const SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar"; // authorization scopes
 const SCOPE_EVENTS = "https://www.googleapis.com/auth/calendar.events";
 
-exports.thePlaceInformer = functions.pubsub.schedule("00 * * * *").onRun(async (context) => {
-  try {
-    const key = readPrivateKey();
-    const auth = await authentificate(key);
-    await getList(auth);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-function readPrivateKey() {
+const readPrivateKey = () => {
   const content = fs.readFileSync(KEYFILE);
   return JSON.parse(content.toString());
-}
+};
 
-async function authentificate(key) {
+const authentificate = async (key) => {
   const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, [SCOPE_CALENDAR, SCOPE_EVENTS]);
   await jwtClient.authorize();
   return jwtClient;
-}
+};
 
-async function getList(auth) {
+const getList = async (auth, smsKey) => {
   const calendar = google.calendar({ version: "v3", auth });
   calendar.events.list(
     { calendarId: CALENDAR_ID, timeMin: new Date().toISOString(), maxResults: 10, singleEvents: true, orderBy: "startTime" },
@@ -37,21 +27,32 @@ async function getList(auth) {
       if (err) return console.log(err);
       const events = res.data.items;
       if (events.length) {
-        const sms = new SMSru("C2288C95-BB2D-97B7-3900-1000BA53715C");
+        const sms = new SMSru(smsKey);
         events.forEach((event) => {
           if (event.description) {
             const start = event.start.dateTime || event.start.date;
             const now = new Date();
             const delta = (new Date(start).getTime() - now.getTime()) / 36e5;
             if (delta < 24 && delta > 23)
-              sms.sms_send({ to: event.description, text:  `Вы записаны` }, (e) => {
+              sms.sms_send({ to: event.description, text: `Вы записаны` }, (e) => {
                 console.log(e.description);
               });
           }
         });
+        return 1;
       } else {
-        console.log("No upcoming events found.");
+        return console.log("No upcoming events found.");
       }
     }
   );
-}
+};
+
+exports.thePlaceInformer = functions.pubsub.schedule("00 * * * *").onRun(async (context) => {
+  try {
+    const key = readPrivateKey();
+    const auth = await authentificate(key);
+    await getList(auth, key.sms_key);
+  } catch (err) {
+    console.log(err);
+  }
+});
