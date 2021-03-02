08bc07d1af06bef0ed69a3df8668061b0e613aa9
kovmike <kovmike@yandex.ru>
2021-03-02 15:16:46 +0300

    secure + upd SMS

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const { google } = require("googleapis");
 const SMSru = require("sms_ru");
 
-const CALENDAR_ID = "010e056vkftbth7gcr9e4t0af8@group.calendar.google.com";
-const KEYFILE = "configCalendar.json"; // path to JSON with private key been downloaded from Google
+//const CALENDAR_ID = "010e056vkftbth7e4t0af8@group.calendar.google.com"; - old
+const KEYFILE = "configCalendar.json";
 const SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar"; // authorization scopes
 const SCOPE_EVENTS = "https://www.googleapis.com/auth/calendar.events";
 
@@ -19,10 +19,10 @@ const authentificate = async (key) => {
   return jwtClient;
 };
 
-const getList = async (auth, smsKey) => {
+const getList = async (auth, smsKey, calendarID) => {
   const calendar = google.calendar({ version: "v3", auth });
   calendar.events.list(
-    { calendarId: CALENDAR_ID, timeMin: new Date().toISOString(), maxResults: 15, singleEvents: true, orderBy: "startTime" },
+    { calendarId: calendarID, timeMin: new Date().toISOString(), maxResults: 15, singleEvents: true, orderBy: "startTime" },
     (err, res) => {
       if (err) return console.log(err);
       const events = res.data.items;
@@ -31,14 +31,19 @@ const getList = async (auth, smsKey) => {
         events.forEach((event) => {
           if (event.description) {
             const start = new Date(event.start.dateTime || event.start.date);
+            const day = start.getDate();
+            const month = start.getMonth() + 1;
             const hour = start.getHours();
-            const minutes = start.getMinutes() < 10 ? "0" + start.getMinutes() : start.getMunutes();
+            const minutes = start.getMinutes() < 10 ? "0" + start.getMinutes() : start.getMinutes();
             const now = new Date();
             const delta = (start.getTime() - now.getTime()) / 36e5;
             if (delta < 24 && delta > 23)
-              sms.sms_send({ to: event.description, text: `Вы записаны в ${hour}:${minutes}`, from: "ThePlace" }, (e) => {
-                console.log(e.description);
-              });
+              sms.sms_send(
+                { to: event.description, text: `Вы записаны на ${day}.${month} в ${hour}:${minutes}`, from: "ThePlace" },
+                (e) => {
+                  console.log(e.description);
+                }
+              );
           }
         });
         return 1;
@@ -53,7 +58,7 @@ exports.thePlaceInformer = functions.pubsub.schedule("00 * * * *").onRun(async (
   try {
     const key = readPrivateKey();
     const auth = await authentificate(key);
-    await getList(auth, key.sms_key);
+    await getList(auth, key.sms_key, key.calendar_id);
   } catch (err) {
     console.log(err);
   }
